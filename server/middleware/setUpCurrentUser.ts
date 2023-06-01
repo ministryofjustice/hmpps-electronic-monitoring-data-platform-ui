3884dea1b2a9676f32a5562ccf56fd265db2ec71
Ben Carter <ben.carter@justice.gov.uk>
2023-06-01 16:07:38 +0100

    Just starting to play around

--- server/middleware/setUpCurrentUser.ts
+++ server/middleware/setUpCurrentUser.ts
@@ -3,8 +3,10 @@ import auth from '../authentication/auth'
 import tokenVerifier from '../data/tokenVerification'
 import populateCurrentUser from './populateCurrentUser'
 import type { Services } from '../services'
+import logger from '../../logger'
 
 export default function setUpCurrentUser({ userService }: Services): Router {
+  logger.info('Setting up user middleware')
   const router = Router({ mergeParams: true })
   router.use(auth.authenticationMiddleware(tokenVerifier))
   router.use(populateCurrentUser(userService))
